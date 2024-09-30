8f44ad2a3e4169a32d040376e898b929222f708b
Soham Palodhy <sohampalodhy8068@gmail.com>
2024-09-30 16:12:02 +0000

    Triangle Program

--- Math_class.java
+++ Math_class.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 public class Math_class {
     public static void main(String[] args) {
-        double x = 3.14;
-        double y = -10;
+        double x,y,z;
 
-        double z = Math.max(x,y);
-        System.out.println(z);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Side X:");
+        x = sc.nextDouble();
+        System.out.println("Enter Side Y:");
+        y = sc.nextDouble();
+
+        z = Math.sqrt((x*x) + (y*y));
+        System.out.println("The Hypotenuse is: " +z);
+
+        sc.close();
     }
 }
