60d9b86739f619f001e694a6ba5be4351348e759
Soham Palodhy <sohampalodhy8068@gmail.com>
2024-09-30 16:11:22 +0000

    Triangle program

---
+++ Math_class.java
@@ -0,0 +1,9 @@
+public class Math_class {
+    public static void main(String[] args) {
+        double x = 3.14;
+        double y = -10;
+
+        double z = Math.max(x,y);
+        System.out.println(z);
+    }
+}
