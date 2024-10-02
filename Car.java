7e51005c6e05fcd2d51519b5f33a9c3b5d289eef
Soham Palodhy <sohampalodhy8068@gmail.com>
2024-10-02 15:42:11 +0000

    Objects Concept

---
+++ Car.java
@@ -0,0 +1,15 @@
+public class Car{
+    String make = "Chevrolet";
+    String Model = "Corvette";
+    int year = 2024;
+    String color = "Blue";
+    double Price = 500000.00;
+
+    void drive(){
+        System.out.println("You drive the car");
+    }
+
+    void brake(){
+        System.out.println("You step on the brakes");
+    }
+}
---
+++ Objects.java
@@ -0,0 +1,9 @@
+public class Objects{
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        System.out.println(myCar.Model);
+
+        myCar.drive();
+        myCar.brake();
+    }
+}
